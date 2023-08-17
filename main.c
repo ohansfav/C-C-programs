b2018bce05ddb46f771a74f27ef2283951ca2a95
Kiara <72391175+ohansfav@users.noreply.github.com>
2023-08-17 18:55:09 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
-int main ()
+#include <stdlib.h>
+#include <math.h>
+//#define PI 3.142
+
+
+int main()
 {
-int n, m, Sum;
-n=25;
-m=30;
-printf ("The value of the first number is %d\n", n);
-//scanf ("%d", &n);
-/*Condition
-Else statement(s) Statement(s)
-True
-False True*/
-printf ("The value of the second number is %d%\n", m);
-//scanf ("%d", &m);
-Sum = n+m;
-printf ("The value of Sum is %d\n", Sum);
-//scanf ("%d", &Sum);
-if (Sum>50)
-{
-printf ("The value of Sum is greater than 50");
-}
-else
-{
-printf ("The value of Sum is less than 50");
-}
-return 0;
+   float r; //r is radius
+   float area;
+   //float PI = 3.142;
+
+   printf("what is radius : \n");
+   scanf("%f", &r);
+
+   area = 3.142 * r * r;
+
+   printf(" area is: %f", area);
+
+
+
+    return 0;
 }
