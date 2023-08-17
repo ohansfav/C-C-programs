195071a56d8f54ffdb35be1d4e2336d6e44a3b78
Kiara <72391175+ohansfav@users.noreply.github.com>
2023-08-17 19:34:03 +0100

    Add files via upload
    counting back from 200 to 1

---
+++ counting 200 to 1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int nat_no = 200;
+    int i = 0;
+
+    while (i<200){
+    printf("the next number is: %d\n", nat_no);
+    nat_no = nat_no - 1;
+    i++;
+    }
+
+    return 0;
+}
