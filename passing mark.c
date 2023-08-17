3da7bc453c64d7383a4975d3309b3377cc09edfc
Kiara <72391175+ohansfav@users.noreply.github.com>
2023-08-17 19:32:46 +0100

    Add files via upload
    check if student pass when passing mark is 50

---
+++ passing mark.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char**argu)
+{
+    /*int x,y,sum;
+    scanf("%d", &x);
+    printf("first number: %d", x);
+
+    scanf("%d", &y);
+    printf("second number: %d", y);
+
+    sum = x + y;
+
+    scanf("%d", &sum);
+    printf("the sum is %d\n", sum);*/
+
+    /*int i = 0;
+    int even = 1;
+
+    while(i<2100){
+            //scanf("%d", &even);
+    printf("the next even number is : %d\n", even);
+    even = even + 1;
+    i++;
+    }*/
+
+    int english, maths, biology, chemistry;
+
+    printf("english score:");
+    scanf("%d", &english);
+
+    printf("maths score:");
+    scanf("%d", &maths);
+
+    printf("biology score:");
+    scanf("%d", &biology);
+
+    printf("chemistry score:");
+    scanf("%d", &chemistry);
+
+   /* if((biology > 49) &&(chemistry > 49) && (english > 50) && (maths > 50 ){
+       printf("admitted");
+       }
+       else{
+        printf("not admitted");
+       };*/
+
+       if((english >= 50 && maths >= 50) && (biology >= 35 && chemistry >=35 )){
+        printf("admitted");
+       }
+       //else if(english <= 50 && maths <= 50){
+        //printf("not admitted");
+       //}
+       else{
+        printf("not admitted");
+       }
+
+    return 0;
+}
