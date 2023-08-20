ad7034619c6dd2522ab932b10f4081026269d0fb
Kiara <72391175+ohansfav@users.noreply.github.com>
2023-08-20 19:50:06 +0100

    Update sumproddiv.c

--- sumproddiv.c
+++ sumproddiv.c
@@ -13,15 +13,15 @@ int main()
  printf("%d \n",r);
 
  printf("sum is %d + %d + %d\n", a,g,r);
- sum=a+g+r;
+ sum=a+g+r; // calculate the sum of the given values
 printf("total sum is %d\n",sum);
 
  printf("prod is %d * %d * %d\n", a,g,r);
- prod=a*g*r;
+ prod=a*g*r; // calculate the product
  printf("total prod is %d\n", prod);
 
  printf("div is %d / %d / %d\n", a,g,r );
- div=a/g/r;
+ div=a/g/r; // for division 
  printf("total div is %d", div);
 
 
