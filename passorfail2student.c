e79372cfd43f086eaf3a63989b306eb87435692e
Kiara <72391175+ohansfav@users.noreply.github.com>
2023-08-20 19:48:22 +0100

    Update passorfail2student.c

--- passorfail2student.c
+++ passorfail2student.c
@@ -10,14 +10,14 @@ int main()
 
 
 printf(" please enter number srtu1:\t"); // first student 
-scanf("%d",&stu1);
+scanf("%d",&stu1); // takes input for first student 
 
 
 printf(" please enter number srtu2:\t"); //second student 
-scanf("%d",&stu2);
+scanf("%d",&stu2); // input for second student 
 
 
-    if ( stu1 >= 50){
+    if ( stu1 >= 50){ // condition if student 1 is equal or above 50
         printf("student 1 pass  score is :%d\n", stu1);
     } 
     else{
