eee967a346b3970619294335d7f60f562296b204
Kiara <72391175+ohansfav@users.noreply.github.com>
2023-08-24 19:46:30 +0100

    Update sumandproduct.c

--- sumandproduct.c
+++ sumandproduct.c
@@ -10,8 +10,8 @@ printf("the value of the first number is: "); // for output
 scanf("%d", &x); // for input
 printf("the value of the second number is: ");// for output
 scanf("%d", &y); // for input
-sum=x+y; // for the sum of the two inputs
-prod=x*y; // product of the input
+sum=x+y; // for the sum(addition)of the two inputs
+prod=x*y; // product(multiplication) of the input
 printf("the value of the sum number is: %d\n" , sum); // the sum 
 //scanf("%d", &sum);
 printf("the value of the prod number is: %d\n" , prod); // the product
