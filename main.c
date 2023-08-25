70c71ce5df1ba8c7e845656a4fab62edea7bf9d7
Kiara <72391175+ohansfav@users.noreply.github.com>
2023-08-25 22:44:17 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <math.h> // for mathematical calculations 
 //#define PI 3.142
 
 
 int main()
 {
    float r; //r is radius
-   float area;
+   float area; // area is float because we might have decimal 
    //float PI = 3.142;
 
-   printf("what is radius : \n");
-   scanf("%f", &r);
+   printf("what is radius : \n");// asks for radius input
+   scanf("%f", &r); // takes radius input
 
-   area = 3.142 * r * r;
+   area = 3.142 * r * r; // formula for area 
 
-   printf(" area is: %f", area);
+   printf(" area is: %f", area); // print the area in float
 
 
 
