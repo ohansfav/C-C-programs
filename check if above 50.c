b9450f38a6b45e9ecef26e92fec039d5a28d8546
Kiara <72391175+ohansfav@users.noreply.github.com>
2023-09-01 21:06:18 +0100

    Update check if above 50.c

--- check if above 50.c
+++ check if above 50.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 int main ()
 {
-int n, m, Sum;
-n=25;
-m=30;
-printf ("The value of the first number is %d\n", n);
+int n, m, Sum; // declaring variables to be used
+n=25; // assigning value to n
+m=30; // assigning value to m
+printf ("The value of the first number is %d\n", n); //displays assigned variables
 //scanf ("%d", &n);
 /*Condition
 Else statement(s) Statement(s)
 True
 False True*/
-printf ("The value of the second number is %d%\n", m);
+printf ("The value of the second number is %d%\n", m); // displays second assigned variables 
 //scanf ("%d", &m);
-Sum = n+m;
+Sum = n+m; //adds the two variable
 printf ("The value of Sum is %d\n", Sum);
 //scanf ("%d", &Sum);
-if (Sum>50)
-{
+if (Sum > 50){
 printf ("The value of Sum is greater than 50");
 }
 else
